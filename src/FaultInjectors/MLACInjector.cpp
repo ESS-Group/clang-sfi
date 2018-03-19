76d9a1e78c6a14544f3c8001144a83e3e754eeca
siegert <siegert@715e533e-1b6e-4a5f-915a-d1258084fdf9>
2018-03-19 19:17:33 +0000

    corrected MLAC and MLOC injectors
    git-svn-id: https://repos.cs.tu-dortmund.de/essvn/studis/BA-Daniel-Siegert/trunk/faultinjector@4860 715e533e-1b6e-4a5f-915a-d1258084fdf9

--- src/FaultInjectors/MLACInjector.cpp
+++ src/FaultInjectors/MLACInjector.cpp
@@ -1,28 +1,34 @@
 void MLACInjector::inject(std::vector<StmtBinding> target, ASTContext &Context){
     int i = 0;
     for(StmtBinding current : target){
+        /*
         if(verbose)
             printStep(current, Context.getSourceManager(), Context.getLangOpts(),2*(i++),target.size()*2);
         else
-            printStep(current, Context.getSourceManager(),i++,target.size()*2);
+            printStep(current, Context.getSourceManager(),2*(i++),target.size()*2);
+        */
+        if(verbose)
+            printStep(current, Context.getSourceManager(), Context.getLangOpts(),i++,target.size());
+        else
+            printStep(current, Context.getSourceManager(),i++,target.size());
         std::string result = inject(current, Context, true);
         if(result.compare("")){
-            cout<<"-Success"<<endl;
+            cout<<"-Success (1/2)"<<endl;
             writeDown(result, i-1);
         } else
-            cerr << "-Failed"<<endl;
+            cerr << "-Failed (1/2)"<<endl;
 
 
-        if(verbose)
-            printStep(current, Context.getSourceManager(), Context.getLangOpts(),2*(i++),target.size()*2);
+        /*if(verbose)
+            printStep(current, Context.getSourceManager(), Context.getLangOpts(),2*((i))-1,target.size()*2);
         else
-            printStep(current, Context.getSourceManager(),i++,target.size()*2);
+            printStep(current, Context.getSourceManager(),2*((i))-1,target.size()*2);*/
         result = inject(current, Context, false);
         if(result.compare("")){
-            cout<<"-Success"<<endl;
+            cout<<"-Success (2/2)"<<endl;
             writeDown(result, i-1);
         } else
-            cerr << "-Failed"<<endl;
+            cerr << "-Failed (2/2)"<<endl;
         //i++;
     }
 }
@@ -30,7 +36,32 @@ std::string MLACInjector::inject(StmtBinding current, ASTContext &Context){
     return "";
 }
 MLACInjector::MLACInjector(){
-    Matcher.addMatcher(binaryOperator(anyOf(hasAncestor(expr(anyOf(hasParent(ifStmt()),hasParent(doStmt()),hasParent(switchStmt()),hasParent(whileStmt())))),hasParent(ifStmt()),hasParent(doStmt()),hasParent(switchStmt()),hasParent(whileStmt()))).bind("FunctionCall"), createStmtHandler("FunctionCall"));
+    //Matcher.addMatcher(binaryOperator(anyOf(hasAncestor(expr(anyOf(hasParent(ifStmt()),hasParent(doStmt()),hasParent(switchStmt()),hasParent(whileStmt())))),hasParent(ifStmt()),hasParent(doStmt()),hasParent(switchStmt()),hasParent(whileStmt()))).bind("FunctionCall"), createStmtHandler("FunctionCall"));
+    Matcher.addMatcher(
+        stmt(
+            switchStmt(hasCondition(anyOf(binaryOperator(), hasDescendant(binaryOperator()))))
+        ).bind("switch"), createStmtHandler("switch")
+    );
+    Matcher.addMatcher(
+        stmt(
+            doStmt(hasCondition(anyOf(binaryOperator(), hasDescendant(binaryOperator()))))
+        ).bind("do"), createStmtHandler("do")
+    );
+    Matcher.addMatcher(
+        stmt(
+            whileStmt(hasCondition(anyOf(binaryOperator(), hasDescendant(binaryOperator()))))
+        ).bind("while"), createStmtHandler("while")
+    );
+    Matcher.addMatcher(
+        stmt(
+            forStmt(hasCondition(anyOf(binaryOperator(), hasDescendant(binaryOperator()))))
+        ).bind("for"), createStmtHandler("for")
+    );
+    Matcher.addMatcher(
+        stmt(
+            ifStmt(hasCondition(anyOf(binaryOperator(), hasDescendant(binaryOperator()))))
+        ).bind("if"), createStmtHandler("if")
+    );
 }
 
 std::string MLACInjector::toString(){
@@ -42,7 +73,7 @@ std::string MLACInjector::inject(StmtBinding current, ASTContext &Context, bool 
     R.setSourceMgr(Context.getSourceManager(), Context.getLangOpts());
     SourceLocation start, end;
     if(left){
-        start = ((const BinaryOperator *)current.stmt)->getOperatorLoc();//.getLocWithOffset(-1);//.getLocWithOffset(1);
+        start = ((const BinaryOperator *)current.stmt)->getOperatorLoc();
         end = ((const BinaryOperator *)current.stmt)->getRHS()->getLocEnd();
     }else {
         start = ((const BinaryOperator *)current.stmt)->getLHS()->getLocStart();
@@ -55,5 +86,27 @@ std::string MLACInjector::inject(StmtBinding current, ASTContext &Context, bool 
     return getEditedString(R, Context);
 }
 bool MLACInjector::checkStmt(const Stmt* stmt, std::string binding, ASTContext &Context){//no else
-    return ((const BinaryOperator *)stmt)->getOpcode() == 18;
+    std::vector<const BinaryOperator*> binaryOperators;
+    if(binding.compare("if")==0){
+        const Expr* condition = ((const IfStmt*)stmt)->getCond();
+        binaryOperators = getChildrenOfType<BinaryOperator>(condition);
+    } else if(binding.compare("do")==0){
+        const Stmt* condition = ((const DoStmt*)stmt)->getCond();
+        binaryOperators = getChildrenOfType<BinaryOperator>(condition);
+    }  else if(binding.compare("switch")==0){
+        const Stmt* condition = ((const SwitchStmt*)stmt)->getCond();
+        binaryOperators = getChildrenOfType<BinaryOperator>(condition);
+    }  else if(binding.compare("while")==0){
+        const Stmt* condition = ((const WhileStmt*)stmt)->getCond();
+        binaryOperators = getChildrenOfType<BinaryOperator>(condition);
+    }  else if(binding.compare("for")==0){
+        const Stmt* condition = ((const ForStmt*)stmt)->getCond();
+        binaryOperators = getChildrenOfType<BinaryOperator>(condition);
+    } else 
+        return false;
+    for(const BinaryOperator* op : binaryOperators){
+        if(op->getOpcode()==BinaryOperatorKind::BO_LAnd)
+            nodeCallback("MLAC",op);
+    }
+    return false;
 }
--- src/FaultInjectors/MLOCInjector.cpp
+++ src/FaultInjectors/MLOCInjector.cpp
@@ -1,5 +1,67 @@
+void MLOCInjector::inject(std::vector<StmtBinding> target, ASTContext &Context){
+    int i = 0;
+    for(StmtBinding current : target){
+        /*
+        if(verbose)
+            printStep(current, Context.getSourceManager(), Context.getLangOpts(),2*(i++),target.size()*2);
+        else
+            printStep(current, Context.getSourceManager(),2*(i++),target.size()*2);
+        */
+        if(verbose)
+            printStep(current, Context.getSourceManager(), Context.getLangOpts(),i++,target.size());
+        else
+            printStep(current, Context.getSourceManager(),i++,target.size());
+        std::string result = inject(current, Context, true);
+        if(result.compare("")){
+            cout<<"-Success (1/2)"<<endl;
+            writeDown(result, i-1);
+        } else
+            cerr << "-Failed (1/2)"<<endl;
+
+
+        /*if(verbose)
+            printStep(current, Context.getSourceManager(), Context.getLangOpts(),2*((i))-1,target.size()*2);
+        else
+            printStep(current, Context.getSourceManager(),2*((i))-1,target.size()*2);*/
+        result = inject(current, Context, false);
+        if(result.compare("")){
+            cout<<"-Success (2/2)"<<endl;
+            writeDown(result, i-1);
+        } else
+            cerr << "-Failed (2/2)"<<endl;
+        //i++;
+    }
+}
+std::string MLOCInjector::inject(StmtBinding current, ASTContext &Context){
+    return "";
+}
 MLOCInjector::MLOCInjector(){
-    Matcher.addMatcher(binaryOperator(anyOf(hasAncestor(expr(anyOf(hasParent(ifStmt()),hasParent(doStmt()),hasParent(switchStmt()),hasParent(whileStmt())))),hasParent(ifStmt()),hasParent(doStmt()),hasParent(switchStmt()),hasParent(whileStmt()))).bind("FunctionCall"), createStmtHandler("FunctionCall"));
+    //Matcher.addMatcher(binaryOperator(anyOf(hasAncestor(expr(anyOf(hasParent(ifStmt()),hasParent(doStmt()),hasParent(switchStmt()),hasParent(whileStmt())))),hasParent(ifStmt()),hasParent(doStmt()),hasParent(switchStmt()),hasParent(whileStmt()))).bind("FunctionCall"), createStmtHandler("FunctionCall"));
+    Matcher.addMatcher(
+        stmt(
+            switchStmt(hasCondition(anyOf(binaryOperator(), hasDescendant(binaryOperator()))))
+        ).bind("switch"), createStmtHandler("switch")
+    );
+    Matcher.addMatcher(
+        stmt(
+            doStmt(hasCondition(anyOf(binaryOperator(), hasDescendant(binaryOperator()))))
+        ).bind("do"), createStmtHandler("do")
+    );
+    Matcher.addMatcher(
+        stmt(
+            whileStmt(hasCondition(anyOf(binaryOperator(), hasDescendant(binaryOperator()))))
+        ).bind("while"), createStmtHandler("while")
+    );
+    Matcher.addMatcher(
+        stmt(
+            forStmt(hasCondition(anyOf(binaryOperator(), hasDescendant(binaryOperator()))))
+        ).bind("for"), createStmtHandler("for")
+    );
+    Matcher.addMatcher(
+        stmt(
+            ifStmt(hasCondition(anyOf(binaryOperator(), hasDescendant(binaryOperator()))))
+        ).bind("if"), createStmtHandler("if")
+    );
 }
 
 std::string MLOCInjector::toString(){
@@ -11,49 +73,39 @@ std::string MLOCInjector::inject(StmtBinding current, ASTContext &Context, bool 
     R.setSourceMgr(Context.getSourceManager(), Context.getLangOpts());
     SourceLocation start, end;
     if(left){
-        start = ((const BinaryOperator *)current.stmt)->getOperatorLoc();//.getLocWithOffset(-1);//.getLocWithOffset(1);
+        start = ((const BinaryOperator *)current.stmt)->getOperatorLoc();
         end = ((const BinaryOperator *)current.stmt)->getRHS()->getLocEnd();
     }else {
         start = ((const BinaryOperator *)current.stmt)->getLHS()->getLocStart();
         end = ((const BinaryOperator *)current.stmt)->getRHS()->getLocStart().getLocWithOffset(-1);
     }
 
-
     SourceRange range(start, end);
     R.RemoveText(range);
     return getEditedString(R, Context);
 }
 bool MLOCInjector::checkStmt(const Stmt* stmt, std::string binding, ASTContext &Context){//no else
-
-    return ((const BinaryOperator *)stmt)->getOpcode() == 19;
-}
-void MLOCInjector::inject(std::vector<StmtBinding> target, ASTContext &Context){
-    int i = 0;
-    for(StmtBinding current : target){
-        if(verbose)
-            printStep(current, Context.getSourceManager(), Context.getLangOpts(),2*(i++),target.size()*2);
-        else
-            printStep(current, Context.getSourceManager(),i++,target.size()*2);
-        std::string result = inject(current, Context, true);
-        if(result.compare("")){
-            cout<<" -Success"<<endl;
-            writeDown(result, i-1);
-        } else
-            cerr << "-Failed"<<endl;
-
-
-        if(verbose)
-            printStep(current, Context.getSourceManager(), Context.getLangOpts(),2*(i++),target.size()*2);
-        else
-            printStep(current, Context.getSourceManager(),i++,target.size()*2);
-        result = inject(current, Context, false);
-        if(result.compare("")){
-            cout<<" -Success"<<endl;
-            writeDown(result, i-1);
-        } else
-            cerr << "-Failed"<<endl;
+    std::vector<const BinaryOperator*> binaryOperators;
+    if(binding.compare("if")==0){
+        const Expr* condition = ((const IfStmt*)stmt)->getCond();
+        binaryOperators = getChildrenOfType<BinaryOperator>(condition);
+    } else if(binding.compare("do")==0){
+        const Stmt* condition = ((const DoStmt*)stmt)->getCond();
+        binaryOperators = getChildrenOfType<BinaryOperator>(condition);
+    }  else if(binding.compare("switch")==0){
+        const Stmt* condition = ((const SwitchStmt*)stmt)->getCond();
+        binaryOperators = getChildrenOfType<BinaryOperator>(condition);
+    }  else if(binding.compare("while")==0){
+        const Stmt* condition = ((const WhileStmt*)stmt)->getCond();
+        binaryOperators = getChildrenOfType<BinaryOperator>(condition);
+    }  else if(binding.compare("for")==0){
+        const Stmt* condition = ((const ForStmt*)stmt)->getCond();
+        binaryOperators = getChildrenOfType<BinaryOperator>(condition);
+    } else 
+        return false;
+    for(const BinaryOperator* op : binaryOperators){
+        if(op->getOpcode()==BinaryOperatorKind::BO_LOr)
+            nodeCallback("MLOC",op);
     }
-}
-std::string MLOCInjector::inject(StmtBinding current, ASTContext &Context){
-    return "";
+    return false;
 }
