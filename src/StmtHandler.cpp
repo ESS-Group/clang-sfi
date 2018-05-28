26d65b36f5a0e43f09df2d0d76c033d9e9b96bfa
siegert <siegert@715e533e-1b6e-4a5f-915a-d1258084fdf9>
2018-05-28 02:57:15 +0000

    removed unnessesary if
    git-svn-id: https://repos.cs.tu-dortmund.de/essvn/studis/BA-Daniel-Siegert/trunk/faultinjector@4970 715e533e-1b6e-4a5f-915a-d1258084fdf9

--- src/StmtHandler.cpp
+++ src/StmtHandler.cpp
@@ -10,24 +10,18 @@ StmtHandler::StmtHandler(FaultInjector *pFaultInjector, std::string name, std::v
 void StmtHandler::run(const MatchFinder::MatchResult &Result){
     for(std::string binding:bindings){
         if(const Stmt *stmt = Result.Nodes.getNodeAs<clang::Stmt>(binding)){
-
             std::string name(Result.Context->getSourceManager().getFilename(stmt->getLocStart()));
             if(faultInjector->getFileName().compare(name)==0){//nur Nodes aus dem zu parsenden File beachten!!
-
-                //stmt->getLocStart().dump(Result.Context->getSourceManager());
-                //cerr<<endl;
                 if(faultInjector->checkStmt(stmt, binding, *Result.Context))//nur Statemets die durch checkStmt erlaubt sind
                     faultInjector->nodeCallback(binding, stmt);
             }
         } else if(const Decl *stmt = Result.Nodes.getNodeAs<clang::Decl>(binding)){
-            if(const Decl *stmt = Result.Nodes.getNodeAs<clang::Decl>(binding)){
                 std::string name(Result.Context->getSourceManager().getFilename(stmt->getLocStart()));
                 if(faultInjector->getFileName().compare(name)==0){//nur Nodes aus dem zu parsenden File beachten!!
                     if(faultInjector->checkStmt(stmt, binding, *Result.Context)){//nur Statemets die durch checkStmt erlaubt sind
                         faultInjector->nodeCallback(binding, stmt);
                     }
                 }
-            }
         }
     }
 }
