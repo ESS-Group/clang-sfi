5059a5634e02ca7986f26d3703a29e7e7568e8f2
Ulrich Thomas Gabor <ulrich.gabor@tu-dortmund.de>
2018-10-12 14:52:35 +0200

    Pass a sane value to make -j for some systems.

--- build.sh
+++ build.sh
@@ -6,5 +6,9 @@ rm -Rf build
 mkdir build
 cd build
 cmake -G "Unix Makefiles" -DLLVM_SRC_DIR=$(realpath ../llvm) -DLLVM_DIR=$(realpath ../llvm-build/lib/cmake/llvm) ..
-make -j
+if [ -x "$(command -v nproc)" ]; then
+	make -j$(nproc)
+else
+	make -j
+fi
 
