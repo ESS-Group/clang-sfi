5a29b888cc0a76ad6d5c4a84a305adb5d5559a0d
Daniel Siegert <daniel.siegert@tu-dortmund.de>
2019-01-16 03:02:38 +0100

    Added header files in CMakeLists.txt on Windows environments so that they are shown in Visual Studio.
    Fixed bug in build_win.sh

--- build_win.sh
+++ build_win.sh
@@ -9,6 +9,6 @@ cd "$(dirname "$0")"
 #rm -Rf VSProject
 mkdir VSProject || true
 cd VSProject
-cmake -G "Visual Studio 15 2017" -A x64 -Thost=x64 -DLLVM_SRC_DIR=$(realpath ../llvm) -DLLVM_DIR=$(realpath ../llvm-build/lib/cmake/llvm) -DZLIB_INCLUDE_DIR=$(realpath ../zlib) -DZLIB_LIBRARY=$(realpath ../zlib-build/$BUILD_TYPE/zlibstatic.lib) ..
+cmake -G "Visual Studio 15 2017" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -A x64 -Thost=x64 -DLLVM_SRC_DIR=$(realpath ../llvm) -DLLVM_DIR=$(realpath ../llvm-build/lib/cmake/llvm) -DZLIB_INCLUDE_DIR=$(realpath ../zlib) -DZLIB_LIBRARY=$(realpath ../zlib-build/$BUILD_TYPE/zlibstatic.lib) ..
 cmake --build . --target clang-sfi --config $BUILD_TYPE
 
