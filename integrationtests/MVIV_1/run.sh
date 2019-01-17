783d3f7d87435e71fe2e38db167c8386c32992f8
Ulrich Thomas Gabor <ulrich.gabor@tu-dortmund.de>
2019-01-17 09:15:29 +0100

    Added test for MVIV for ticket #2.

---
+++ integrationtests/MVIV_1/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ue
+cd "$(dirname "$0")"
+
+# $2 is CMAKE_CURRENT_SOURCE_DIR
+# $3 is CMAKE_CURRENT_BINARY_DIR
+OUTPUT_FILE=$3/output.txt
+REFERENCE_FILE=$2/reference.txt
+
+$1 $2/source.cpp --config=$2/config.json --dir=$3/injections -- > $OUTPUT_FILE
+{ tail -n +3 injections/MVIV_0.patch & tail -n +3 injections/MVIV_1.patch; } | diff $REFERENCE_FILE - >> $OUTPUT_FILE || (echo "Test failed, reference output not given" && exit 1)
---
+++ integrationtests/MVIV_1/source.cpp
@@ -0,0 +1,4 @@
+int main(int argc, char ** argv) {
+	int b = 5, c = 6;
+	return 0;
+}
