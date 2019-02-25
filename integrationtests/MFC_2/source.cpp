41aad3bf32ced01565640c109ea43410c108cfba
Ulrich Thomas Gabor <ulrich.gabor@tu-dortmund.de>
2019-02-25 14:50:05 +0100

    MFC unit test for macro matching

---
+++ integrationtests/MFC_2/source.cpp
@@ -0,0 +1,17 @@
+#define callf f
+
+int f() {
+	return 1;
+}
+int main(int argc, char ** argv) {
+	int a = f();
+	int b = 5, c = 6;
+	int e;
+	e = a + b + c;
+	callf();
+	if (e) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
