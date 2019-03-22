a1a8cab8a352ee69a1f4435397abfa04b714bed3
Ulrich Thomas Gabor <ulrich@ulrichgabor.de>
2019-03-22 13:03:39 +0100

    Fixed integration test MVIV to run on multiple platforms.

--- integrationtests/MVIV_2/run.sh
+++ integrationtests/MVIV_2/run.sh
@@ -12,4 +12,6 @@ OUTPUT_FILE=$BUILD_DIR/output.txt
 REFERENCE_FILE=$SOURCE_DIR/reference.txt
 
 $EXECUTABLE $SOURCE_DIR/source.cpp --config=$SOURCE_DIR/config.json --dir=$BUILD_DIR/injections -- > $OUTPUT_FILE
-ls $BUILD_DIR/injections/* | wc -l | diff $REFERENCE_FILE - >> $OUTPUT_FILE || (echo "Test failed, reference output not given" && exit 1)
+WC_OUTPUT=$(ls $BUILD_DIR/injections/* | wc -l)
+WC_OUTPUT_TRIMMED="$(echo -e "${WC_OUTPUT}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+echo $WC_OUTPUT_TRIMMED | diff $REFERENCE_FILE - >> $OUTPUT_FILE || (echo "Test failed, reference output not given" && exit 1)
