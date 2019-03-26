b8d2ec7b06a37918327031904cf0e35a2461ba5f
Ulrich Thomas Gabor <ulrich.gabor@tu-dortmund.de>
2019-03-26 14:10:12 +0100

    Merged build.sh+build_win.sh and setup-clang.sh+setup-clang_win.sh.

--- build.sh
+++ build.sh
@@ -2,13 +2,24 @@
 set -ue
 cd "$(dirname "$0")"
 
-rm -Rf build
-mkdir build
-cd build
-cmake -G "Unix Makefiles" -DLLVM_SRC_DIR=$(realpath ../llvm) -DLLVM_DIR=$(realpath ../llvm-build/lib/cmake/llvm) ..
-if [ -x "$(command -v nproc)" ]; then
-	make -j$(nproc)
-else
-	make -j
+BUILD_TYPE=Release # or Debug
+if [[ $(uname -s) == CYGWIN* ]] || [[ $(uname -s) == MINGW* ]]; then
+    WINDOWS=true
 fi
 
+if [ ! -z ${WINDOWS+x} ]; then
+	mkdir VSProject || true
+	cd VSProject
+	cmake -G "Visual Studio 15 2017" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -A x64 -Thost=x64 -DLLVM_SRC_DIR=$(realpath ../llvm) -DLLVM_DIR=$(realpath ../llvm-build/lib/cmake/llvm) -DZLIB_INCLUDE_DIR=$(realpath ../zlib) -DZLIB_LIBRARY=$(realpath ../zlib-build/$BUILD_TYPE/zlibstatic.lib) ..
+	cmake --build . --target clang-sfi --config $BUILD_TYPE
+else
+	rm -Rf build
+	mkdir build
+	cd build
+	cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DLLVM_SRC_DIR=$(realpath ../llvm) -DLLVM_DIR=$(realpath ../llvm-build/lib/cmake/llvm) ..
+	if [ -x "$(command -v nproc)" ]; then
+		make -j$(nproc)
+	else
+		make -j
+	fi
+fi
--- setup-clang.sh
+++ setup-clang.sh
@@ -3,6 +3,9 @@ set -ue
 
 TOP_LEVEL_DIRECTORY=$(dirname "$(realpath "$0")")
 BUILD_TYPE=Release # or Debug
+if [[ $(uname -s) == CYGWIN* ]] || [[ $(uname -s) == MINGW* ]]; then
+    WINDOWS=true
+fi
 
 cd "$TOP_LEVEL_DIRECTORY"
 echo "Switched into $(pwd)"
@@ -11,6 +14,12 @@ cd llvm/tools
 git clone https://llvm.org/git/clang.git || true
 (cd clang && git reset --hard 0513b409d5e34b2d2a28ae21b6d620cc52de0e57)
 
+if [ ! -z ${WINDOWS+x} ]; then
+    cd "$TOP_LEVEL_DIRECTORY"
+    echo "Checking out zlib"
+    git clone https://github.com/madler/zlib.git || true
+    (cd zlib && git reset --hard cacf7f1d4e3d44d871b605da3b647f07d718623f)
+fi
 
 cd "$TOP_LEVEL_DIRECTORY"
 echo "Checking out dtl"
@@ -24,8 +33,12 @@ echo "Switched into $(pwd)"
 mkdir -p llvm-build
 cd llvm-build
 echo "Created llvm-build"
-cmake -G Ninja ../llvm -DCMAKE_BUILD_TYPE=$BUILD_TYPE
-echo "Preparing Ninja files"
+if [ ! -z ${WINDOWS+x} ]; then
+    cmake -G "Visual Studio 15 2017" -A x64 -Thost=x64 -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DZLIB_INCLUDE_DIR=$(realpath ../zlib) -DZLIB_LIBRARY=$(realpath ../zlib-build/$BUILD_TYPE/zlibstatic.lib) ../llvm
+else
+    cmake -G Ninja ../llvm -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+fi
+echo "Ran CMake"
 
 COMPILE="n"
 read -p "Should I start the compile? [$COMPILE]: " userInput
@@ -35,6 +48,10 @@ then
 fi
 if [[ $COMPILE =~ ^(yes|y|Y) ]]; then
     echo "Start compile"
-    ninja
+    if [ ! -z ${WINDOWS+x} ]; then
+        cmake --build . --target ALL_BUILD --config $BUILD_TYPE
+    else
+        ninja
+    fi
 fi
 
