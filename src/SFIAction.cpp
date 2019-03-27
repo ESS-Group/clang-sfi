0f99b136a85a8e8429095502961afcbce2e1cad9
Ulrich Thomas Gabor <ulrich@ulrichgabor.de>
2019-03-27 22:25:23 +0100

    Cleaned up code.

--- src/SFIAction.cpp
+++ src/SFIAction.cpp
@@ -9,16 +9,18 @@ using namespace clang::ast_matchers;
 
 SFIAction::SFIAction(std::vector<FaultInjector *> injs) : injectors(injs) {
 }
+
 void SFIAction::EndSourceFileAction() {
     std::cout << "Parsed file  " << fileName << std::endl;
 }
+
 std::unique_ptr<ASTConsumer> SFIAction::CreateASTConsumer(CompilerInstance &CI, StringRef file) {
-    fileName = file.data();
+    fileName = file.str();
     std::cout << "Parsing file '" << fileName << "'" << std::endl;
 
     // Do not print warnings:
     // CI.getDiagnostics().setClient(new IgnoringDiagConsumer());
 
     // Rewrite.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
-    return llvm::make_unique<SFIASTConsumer>(file, injectors);
+    return llvm::make_unique<SFIASTConsumer>(fileName, injectors);
 }
