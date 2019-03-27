a62e15c49469ef7ce41de07ae1ed7c0a08bd59a0
Ulrich Thomas Gabor <ulrich@ulrichgabor.de>
2019-03-27 14:52:47 +0100

    Fixed CLI call of integration test MFC_3.

--- integrationtests/MFC_3/run.sh
+++ integrationtests/MFC_3/run.sh
@@ -11,5 +11,5 @@ source ../init.sh
 OUTPUT_FILE=$BUILD_DIR/output.txt
 REFERENCE_FILE=$SOURCE_DIR/reference.txt
 
-$EXECUTABLE $SOURCE_DIR/source.cpp --config=$SOURCE_DIR/config.json --dir=$BUILD_DIR/injections -p ../../build -- > $OUTPUT_FILE
+$EXECUTABLE $SOURCE_DIR/source.cpp --config=$SOURCE_DIR/config.json --dir=$BUILD_DIR/injections -p ../../build > $OUTPUT_FILE
 tail -n +3 $BUILD_DIR/injections/MFC_0.patch | diff $REFERENCE_FILE - >> $OUTPUT_FILE || (echo "Test failed, reference output not given" && exit 1)
